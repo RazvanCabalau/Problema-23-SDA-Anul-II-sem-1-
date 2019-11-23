d1bf970570ef268370b6e6c54205d992ac847b5d
RazvanCabalau <57367335+RazvanCabalau@users.noreply.github.com>
2019-11-23 16:43:14 +0200

    Update HFY.py

--- HFY.py
+++ HFY.py
@@ -1,7 +1,5 @@
 from problem import Problem
 import random
-from itertools import chain, combinations
-import collections
 class Problema23(Problem):
     def __init__(self):
         n = random.randint(8, 12)
@@ -9,7 +7,6 @@ class Problema23(Problem):
         data = (datagen)
         statement = 'Primiti vectorul: ' + ', '.join(map(str, data[0])) + '. '
         statement += 'Faceti din acest vector un min-ansamblu folosind\n un numar minim de operatii si demonstrati complexitatea.\n'
-        statement += '\n'
 
         super().__init__(statement, data)
 
